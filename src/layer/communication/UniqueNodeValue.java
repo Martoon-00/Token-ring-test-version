6a125fdbb972e7d9a86f125a6df12bdb8b60b1be
Martoon-00 <Martoon_00@mail.ru>
2015-10-15 00:06:51 +0300

    Doc updated

--- src/layer/communication/UniqueNodeValue.java
+++ src/layer/communication/UniqueNodeValue.java
@@ -2,6 +2,9 @@ package layer.communication;
 
 import java.io.Serializable;
 
+/**
+ * Sort of priority for a node
+ */
 public class UniqueNodeValue implements Serializable, Comparable<UniqueNodeValue> {
     @Override
     public int compareTo(UniqueNodeValue o) {
